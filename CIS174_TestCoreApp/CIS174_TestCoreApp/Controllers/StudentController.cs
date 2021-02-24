5fd4c22618fc3958dc3c624ec278eba303854ec4
IWertz <77807438+IWertz@users.noreply.github.com>
2021-02-24 01:24:37 -0600

    Added Content

---
+++ CIS174_TestCoreApp/CIS174_TestCoreApp/Controllers/StudentController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CIS174_TestCoreApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CIS174_TestCoreApp.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult StudentView(StudentList studentList, int id)
+        {
+            var students = studentList.setList();
+            ViewBag.AccessLevel = id;
+            return View(students);
+        }
+    }
+}
---
+++ CIS174_TestCoreApp/CIS174_TestCoreApp/Models/StudentList.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CIS174_TestCoreApp.Models
+{
+    public class StudentList
+    {
+        public List<Student> setList()
+        {
+            Student Isaiah = new Student();
+            Isaiah.FirstName = "Isaiah";
+            Isaiah.LastName = "Wertz";
+            Isaiah.Grade = "A-";
+
+            Student John = new Student();
+            John.FirstName = "John";
+            John.LastName = "Doe";
+            John.Grade = "C";
+
+            Student Adam = new Student();
+            Adam.FirstName = "Adam";
+            Adam.LastName = "Smith";
+            Adam.Grade = "B+";
+
+            var students = new List<Student>().ToList();
+            students.Add(Isaiah);
+            students.Add(John);
+            students.Add(Adam);
+            return students;
+        }
+    }
+}
